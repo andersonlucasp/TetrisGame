3e7243a369a1ca75481bad217f6fe33b46391dde
andersonlucasp <andersonlucaspz@gmail.com>
2020-12-05 20:29:53 -0300

    teste de colisão

--- js/piece.js
+++ js/piece.js
@@ -29,8 +29,55 @@ class Piece {
     }
 
     moveDown() {
-        this.unDraw();
-        this.y++;
-        this.draw();
+        if (!this.collision(0, 1, this.activePiece)) {
+            this.unDraw();
+            this.y++;
+            this.draw();
+            return;
+        }
+        this.lock();
+        piece = randomPiece();
+
     };
+
+    collision(x, y, futurePiece) {
+        for (let currentRow = 0; currentRow < futurePiece.length; currentRow++) {
+            for (let currentCol = 0; currentCol < futurePiece.length; currentCol++) {
+                if (!futurePiece[currentRow][currentCol]) {
+                    continue;
+                }
+
+                let newX = this.x + currentCol + x;
+                let newY = this.y + currentRow + y;
+
+                if (newX < 0 || newX >= COL || newY >= ROW) {
+                    return true;
+                }
+
+                if (newY < 0) {
+                    continue;
+                }
+
+                if (board[newY][newX] != defaultColor) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    lock() {
+        for (let currentRow = 0; currentRow < this.activePiece.length; currentRow++) {
+            for (let currentCol = 0; currentCol < this.activePiece.length; currentCol++) {
+                if (!this.activePiece[currentRow][currentCol]) {
+                    continue;
+                }
+                if (this.y + currentRow < 0) {
+                    break;
+                }
+                board[this.y + currentRow][this.x + currentCol] = this.color;
+            }
+        }
+        drawBoard();
+    }
 };
